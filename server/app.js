c099e1ba6d047598792e14c9f95b4d5d036f8ef0
Jose Guillermo Saldana <memo.sal98@gmail.com>
2019-11-29 23:57:58 -0600

    Changes parser

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const express = require('express'),
 require('./db/dbSetup')
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Serves build
